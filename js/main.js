53032ff223d9577d4cb8a891099c7d6fb52e0045
Alastair Jones <alastair@augmentary.co.uk>
2024-06-14 00:18:42 +0100

    Ensure most recently opened marker appears at front of marker stack, always fit bounds, even if an error occurs

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 let map;
-let bounds;
+let openMarkers = 0;
 
 async function initMap() {
     const [key, entities] = window.location.hash.split('|||');
     const isLatLng = /-?\d+?.\d+?,-?\d+?.\d+?/;
 
-    if(!key || !entities) {
+    if (!key || !entities) {
         return;
     }
 
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    const { Geocoder } = await google.maps.importLibrary("geocoding");
+    const {Map} = await google.maps.importLibrary("maps");
+    const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
+    const {Geocoder} = await google.maps.importLibrary("geocoding");
     const geocoder = new Geocoder();
     const bounds = new google.maps.LatLngBounds();
 
@@ -23,14 +23,23 @@ async function initMap() {
             position,
         });
 
-        marker.addListener('click', function() {
-            marker.content.classList.toggle('open');
+        marker.addListener('click', function () {
+            const classList = marker.content.classList;
+            if (classList.contains('open')) {
+                classList.remove('open');
+                openMarkers -= 1
+                marker.zIndex = null;
+            } else {
+                classList.add('open');
+                openMarkers += 1
+                marker.zIndex = 10 + openMarkers;
+            }
         });
 
         markers.push(marker);
     }
 
-    const position = { lat: 54.00366, lng: -2.547855 };
+    const position = {lat: 54.00366, lng: -2.547855};
     map = new Map(document.getElementById("map"), {
         zoom: 4,
         center: position,
@@ -40,23 +49,25 @@ async function initMap() {
     const markers = [];
     const promises = [];
 
-    for(let entity of entities.split('|:|')) {
-        console.log(entity);
+    for (let entity of entities.split('|:|')) {
         let [address, background, icon, details] = decodeURIComponent(entity).split('|;|');
 
-        if(isLatLng.test(address)) {
+        if (isLatLng.test(address)) {
             const position = new google.maps.LatLng(...address.split(','));
             createMarker(map, buildContent(background, icon, details), position);
         } else {
-            promises.push(geocoder.geocode({ address }).then(function ({ results }) {
+            promises.push(geocoder.geocode({address}).then(function ({results}) {
                 const position = results[0].geometry.location;
                 createMarker(map, buildContent(background, icon, details), position);
             }));
         }
     }
 
-    await Promise.all(promises);
-    map.fitBounds(bounds);
+    try {
+        await Promise.all(promises);
+    } finally {
+        map.fitBounds(bounds);
+    }
 }
 
 function buildContent(background, icon, details) {
